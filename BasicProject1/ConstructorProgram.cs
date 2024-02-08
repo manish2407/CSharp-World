f177ec5b8cfc95b18944193befc198d5062bc340
Manish Sharma <manishms2407@gmail.com>
2024-02-08 23:14:35 +0530

    Basic structure of program in C# and default Constructor in C#

---
+++ BasicProject1/ConstructorProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProject1
+{
+    // 2. Default/Implicit Constructor program
+    internal class ConstructorProgram
+    {
+        int num1;
+        string str;
+        bool boolVariable;
+        static void Main(string[] args)
+        {
+            ConstructorProgram obj = new ConstructorProgram();
+            Console.WriteLine(obj.num1);
+            Console.WriteLine(obj.str);
+            Console.WriteLine(obj.boolVariable);
+
+            Console.ReadLine();
+        }
+    }
+}
--- BasicProject1/FirstProgram.cs
+++ BasicProject1/FirstProgram.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace BasicProject1
 {
+    // 1.Basic Program of C#
     internal class FirstProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello C#");
+
+            Console.ReadLine();
         }
     }
 }
