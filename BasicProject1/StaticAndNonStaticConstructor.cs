61c1f12c28dfae647cb083612a482d8dae311e62
Manish Sharma <manishms2407@gmail.com>
2024-05-10 12:55:41 +0530

    C# Tutorial | Difference between Static and Non-Static Constructor

---
+++ BasicProject1/StaticAndNonStaticConstructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassAndConstructor
+{
+    public class StaticAndNonStaticConstructor
+    {
+        static int x;
+        public int y;
+        static StaticAndNonStaticConstructor()
+        {
+            Console.WriteLine("Static Constructor is called");
+        }
+        public StaticAndNonStaticConstructor()
+        {
+            
+            Console.WriteLine("Non static constructor is called");
+        }
+        public StaticAndNonStaticConstructor(int y) {
+            this.y = y;
+            Console.WriteLine("Non static constructor is called");
+        }
+        
+        static void Main()
+        {
+            StaticAndNonStaticConstructor obj = new StaticAndNonStaticConstructor();
+            StaticAndNonStaticConstructor obj1 = new StaticAndNonStaticConstructor(100);
+            Console.WriteLine(x);
+            Console.WriteLine(obj1.y);
+            Console.ReadLine();
+        }
+    }
+}
