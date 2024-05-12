3dc82699b77d25a75feffb68d7b2246eb4caf374
Manish Sharma <manishms2407@gmail.com>
2024-05-12 19:17:00 +0530

    C# Tutorial | Inheritance Basic, Types , Object Class and Inheriting Parameterised constructor

---
+++ InheritanceClass/InheritanceChildClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class InheritanceChildClass : InheritanceClass
+    {
+        public InheritanceChildClass(int x) : base(100)
+        {
+            Console.WriteLine("Child Class Constructor is called");
+            Console.WriteLine("Value of X is " + x);
+        }
+        public void Function3()
+        {
+            Console.WriteLine("Function 3 called");
+        }
+        static void Main(string[] args)
+        {
+            /*InheritanceChildClass obj = new InheritanceChildClass();
+            obj.Function1();
+            obj.Function2();
+            obj.Function3();*/
+
+            /*InheritanceClass obj;
+            InheritanceChildClass obj2 = new InheritanceChildClass();
+            obj = obj2;
+            obj.Function1();
+            obj.Function2();*/
+
+            InheritanceChildClass obj = new InheritanceChildClass(10);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InheritanceClass/InheritanceClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class InheritanceClass
+    {
+        public InheritanceClass(int y)
+        {
+            Console.WriteLine("Parent class Constructor is called");
+            Console.WriteLine("Value of Y is " + y);
+        }
+        public void Function1() {
+            Console.WriteLine("Function 1 called");
+        }
+        public void Function2()
+        {
+            Console.WriteLine("Function 2 called");
+        }
+        
+    }
+}
