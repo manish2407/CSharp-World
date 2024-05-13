76a163c26a0c0b042875c2c1d89477f9617ba6f6
Manish Sharma <manishms2407@gmail.com>
2024-05-14 00:55:56 +0530

    C# Tutorial | Method Overloading and brief of Polymorphism

---
+++ MethodOverloading/OverLoading.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class OverLoading
+    {
+        public void Function1()
+        {
+            Console.WriteLine("First method called");
+        }
+        public void Function2(int i)
+        {
+            Console.WriteLine("Second method called");
+        }
+        public void Function3(string s)
+        {
+            Console.WriteLine("Third method called");
+        }
+        public void Function4(int i, string s) {
+            Console.WriteLine("Fourth method called");
+        }
+
+        public void Function5(string s, int i)
+        {
+            Console.WriteLine("Fifth method called");
+        }
+        static void Main(string[] args)
+        {
+            OverLoading obj = new OverLoading();
+            obj.Function1 ();
+            obj.Function2(100);
+            obj.Function3("hello");
+            obj.Function4(100, "hello");
+            obj.Function5("hello", 100);
+            Console.ReadLine();
+        }
+    }
+}
