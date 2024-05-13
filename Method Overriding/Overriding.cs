b9df3de9d27f4278e32a70d2410bccf851617acb
Manish Sharma <manishms2407@gmail.com>
2024-05-14 01:27:00 +0530

    C# Tutorial | Method Overriding basics

---
+++ Method Overriding/Overriding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    public class Overriding
+    {
+        public void show()
+        {
+            Console.WriteLine("Parent's show method is called");
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("Parent's test method is called");
+        }
+    }  
+}
---
+++ Method Overriding/OverridingChildClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    public class OverridingChildClass : Overriding
+    {
+        public void show(int i){
+            Console.WriteLine("Child's show method is called");
+        }
+
+        public void Test(string s)
+        {
+           Console.WriteLine("Parent's test method is called");
+        }
+        static void Main(string[] args)
+        {
+
+            OverridingChildClass obj = new OverridingChildClass();
+            obj.show();
+            obj.show(2);
+            obj.Test();
+            obj.Test("hello");
+            Console.ReadLine();
+        }
+    }
+}
