220bf351c3f643f9c582359dabb8ea6710d95139
Manish Sharma <manishms2407@gmail.com>
2024-05-18 15:42:38 +0530

    C# Tutorial | Polymorphism - Operator Overloading

---
+++ OperatorOverloading/OperatorOverloading.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    internal class OperatorOverloading
+    {
+        public void Function1(int x, int y)
+        {
+            int z = x + y;
+            Console.WriteLine(z);
+        }
+        public void Function2(string s1, string s2)
+        {
+            string str = s1 + s2;
+            Console.WriteLine(str);
+        }
+
+        static void Main(string[] args)
+        {
+            OperatorOverloading obj = new OperatorOverloading();
+            obj.Function1(10, 20);
+            obj.Function2("Hello", "C#");
+            Console.ReadLine();
+        }
+    }
+}
