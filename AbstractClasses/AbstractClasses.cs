c6863a9e6504795b847be564243f4b5956748887
Manish Sharma <manishms2407@gmail.com>
2024-05-19 13:53:09 +0530

    C# Tutorial | Abstract Class and Abstract Methods

---
+++ AbstractClasses/AbstractClasses.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    abstract class AbstractClasses
+    {
+        public void Add(int FirstNum, int SecondNum) {
+            Console.WriteLine(FirstNum + SecondNum);
+        }
+        public void Sub(int FirstNum, int SecondNum)
+        {
+            Console.WriteLine(FirstNum - SecondNum);
+        }
+        public abstract void Mul(int FirstNum, int SecondNum);
+        public abstract void Div(int FirstNum, int SecondNum);
+    }
+}
---
+++ AbstractClasses/ImplementationClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class ImplementationClass : AbstractClasses
+    {
+        public override void Mul(int FirstNum, int SecondNum)
+        {
+            Console.WriteLine(FirstNum * SecondNum);
+        }
+        public override void Div(int FirstNum, int SecondNum)
+        {
+            Console.WriteLine(FirstNum / SecondNum);
+        }
+
+        static void Main(string[] args)
+        {
+            ImplementationClass obj = new ImplementationClass();
+            AbstractClasses obj1 = obj;
+            obj1.Add(10, 10);
+            obj.Add(20, 10);
+            obj.Sub(20, 10);
+            obj.Mul(20, 10);
+            obj.Div(20, 10);
+            Console.ReadLine();
+
+        }
+    }
+}
