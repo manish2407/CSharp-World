3e17d097c901c0aa932707d9b4ad9bdaf82956fb
Manish Sharma <manishms2407@gmail.com>
2024-07-30 23:57:57 +0530

    C# Tutorial | Indexers Its implementation and get Accesser, set accesser

---
+++ Indexers/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class Employee
+    {   int ENo;
+        double Salary;
+        string EName, job, DName, Location;
+
+        public Employee(int ENo, double Salary, string EName, string job, string DName, string Location)
+        {
+            this.ENo = ENo;
+            this.Salary = Salary;
+            this.EName = EName;
+            this.job = job;
+            this.DName = DName;
+            this.Location = Location;
+        }
+
+        //indexer
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return ENo;
+            else if(index == 1)
+                    return Salary;
+                else if (index == 2)
+                    return EName;
+                else if (index == 3)
+                    return job;
+                else if (index == 4)
+                    return DName;
+                else if(index == 5)
+                    return Location;
+                return null;
+            }
+            set
+            {
+                if(index == 0)
+                {
+                    ENo = (int)value;
+                }
+                else if(index == 1)
+                {
+                    Salary = (Double)value;
+                }
+                else if (index == 2)
+                {
+                    EName = (string)value;
+                }
+                else if (index == 3)
+                {
+                    job = (string)value;
+                }
+                else if (index ==4)
+                {
+                    DName = (string)value;
+                }
+                else if (index == 5){
+                    Location = (string)value;
+                }
+            }
+        }
+    }
+}
---
+++ Indexers/ImplementationClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class ImplementationClass
+    {
+        static void Main(string[] args)
+        {
+            Employee instance = new Employee(101, 20000, "John", "Manager","Bayers","California");
+            Console.WriteLine("Emp No. is : " + instance[0]);
+            Console.WriteLine("Emp Salary is : " + instance[1]);
+            Console.WriteLine("Emp Name is : " + instance[2]);
+            Console.WriteLine("Emp Job is : " + instance[3]);
+            Console.WriteLine("Emp Last name is : " + instance[4]);
+            Console.WriteLine("Emp Location is : " + instance[5]);
+            instance[3] = "Senior Manager";
+            instance[1] = 36000.00;
+
+            Console.WriteLine("After Updation");
+            Console.WriteLine("Emp No. is : " + instance[0]);
+            Console.WriteLine("Emp Salary is : " + instance[1]);
+            Console.WriteLine("Emp Name is : " + instance[2]);
+            Console.WriteLine("Emp Job is : " + instance[3]);
+            Console.WriteLine("Emp Last name is : " + instance[4]);
+            Console.WriteLine("Emp Location is : " + instance[5]);
+
+            Console.ReadLine();
+        }
+    }
+}
