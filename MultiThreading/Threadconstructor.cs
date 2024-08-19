bf9a55acdd9b42578acca5d05cfa2bc19f0148ff
Manish Sharma <manishms2407@gmail.com>
2024-08-19 10:15:39 +0530

    C# Tutorial | Thread COnstructor, Its declaration and Implementation

--- MultiThreading/Threadconstructor.cs
+++ MultiThreading/Threadconstructor.cs
@@ -18,9 +18,31 @@ namespace MultiThreading
             }
             Console.WriteLine("Thread 1 is exiting");
         }
+
+        public static void Test2(object max)
+        {
+            int num = Convert.ToInt32(max);
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Test 2: " + i);
+
+            }
+            Console.WriteLine("Thread 2 is exiting");
+        }
         public static void Main(string[] args) {
-            Test1();
-            //Thread t = new Thread();
+            //Test1();
+            // ThreadStart for the method without any parameter
+            //ThreadStart obj = new ThreadStart(Test1);
+            //ThreadStart obj = Test1;
+            //ThreadStart obj = delegate { Test1(); };
+            //ThreadStart obj = () => Test1 ();
+
+            // ParameterisedThreadStart for the method with parameter
+            
+            ParameterizedThreadStart obj = new ParameterizedThreadStart(Test2);
+            Thread t = new Thread(obj);
+            //It is not the typesafe as it is taking object as a parameter
+            t.Start(100);
             Console.ReadLine();
         }
     }
