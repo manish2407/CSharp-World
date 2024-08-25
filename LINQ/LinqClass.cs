f9e2e9b279afcb2b1a61d3227a051904c7ce7e8f
Manish Sharma <manishms2407@gmail.com>
2024-08-25 17:11:25 +0530

    C# Tutorial | LINQ and Its implementation

---
+++ LINQ/LinqClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class LinqClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 12, 49, 37, 89, 93, 56, 3, 45, 43, 89, 23, 67, 56, 23, 45, 67, 78 };
+            var brr = from i in arr where i > 40 orderby i descending select i;
+
+            foreach (var b in brr)
+            {
+                Console.Write(b + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 49, 37, 89, 93, 56, 3, 45, 43, 89, 23, 67, 56, 23, 45, 67, 78 };
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 40) count++;
+            }
+            int[]brr = new int[count];
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 40)
+                {
+                    brr[index] =arr[i];
+                    index++;
+                }
+            }
+            Array.Sort(brr);
+            Array.Reverse(brr);
+
+            foreach (int i in brr) { 
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
